ddab696273cc325ed3799ebd6a69c5875b1d25e2
Anıl Yekta <37214615+yek-ta@users.noreply.github.com>
2021-07-18 17:58:28 +0300

    Add files via upload

---
+++ ecicek/C#/ecicek/Form1.Designer.cs
@@ -0,0 +1,341 @@
+﻿namespace ecicek
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label13 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.button6 = new System.Windows.Forms.Button();
+            this.label14 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.BaudRate = 115200;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(60, 137);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(160, 21);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(278, 137);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(97, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Bağlan";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(278, 177);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(97, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Bağlantıyı Kes";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(93, 182);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 3;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(60, 217);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(160, 20);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.Text = "Yeni Kablosuz Ağ Adı";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(60, 265);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(160, 20);
+            this.textBox2.TabIndex = 5;
+            this.textBox2.Text = "Yeni Kablosuz Ağ Şifresi";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(300, 217);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Gönder";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(300, 265);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Gönder";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 405);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 8;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 300;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(57, 240);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(160, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "En fazla 20 karakter girebilirsiniz.";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(8, 355);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(86, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Baglanilan Wi-Fi:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(8, 368);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(65, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Wi-Fi Sifresi:";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(153, 14);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(130, 13);
+            this.label10.TabIndex = 17;
+            this.label10.Text = "İstanbul Aydın Üniversitesi";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(172, 27);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(92, 13);
+            this.label11.TabIndex = 18;
+            this.label11.Text = "Anadolu BİL MYO";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(142, 40);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(151, 13);
+            this.label12.TabIndex = 19;
+            this.label12.Text = "Elektronik Teknolojisi Programı";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(60, 319);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(130, 23);
+            this.button5.TabIndex = 21;
+            this.button5.Text = "Güncelle";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(8, 381);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(88, 13);
+            this.label13.TabIndex = 22;
+            this.label13.Text = "Bağlantı Durumu:";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(11, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(114, 112);
+            this.pictureBox1.TabIndex = 23;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox2.BackgroundImage")));
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox2.Location = new System.Drawing.Point(312, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(114, 112);
+            this.pictureBox2.TabIndex = 24;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(198, 62);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(42, 13);
+            this.label9.TabIndex = 25;
+            this.label9.Text = "e-çiçek";
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(245, 319);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(130, 23);
+            this.button6.TabIndex = 26;
+            this.button6.Text = "Kontrol Sağla";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(60, 288);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(160, 13);
+            this.label14.TabIndex = 27;
+            this.label14.Text = "En fazla 10 karakter girebilirsiniz.";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(438, 437);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.Text = "e-çiçek";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Label label14;
+    }
+}
+
---
+++ ecicek/C#/ecicek/Form1.cs
@@ -0,0 +1,157 @@
+﻿//C# kütüphaneleri
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO.Ports; //Seri haberleşme port kütüphanesi (USB)
+using System.IO; //Seri haberleşme kütüphanesi
+
+namespace ecicek
+{
+    public partial class Form1 : Form
+    {
+        int cicekinternetebagli; //İnternete bağlı olup olmadığını tutan değişken
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e) //Program başladığında
+        {
+            comboBox1.DataSource = SerialPort.GetPortNames(); //Seriportların bütün hepsini combobox a yaz.
+        }
+
+        private void button1_Click(object sender, EventArgs e) //Bağlan butonu
+        {
+            if (!serialPort1.IsOpen) //Bağlantı yapılmadı ise (üst üste binip hata vermesin)
+            {
+                if (comboBox1.Text != "") //Combobox ta seçili var ise
+                {
+                    serialPort1.PortName = comboBox1.Text; //Port adını comboboxtan al
+                    serialPort1.BaudRate = 115200; //Rate ayarı(WeMos için seri port 115200
+                    serialPort1.Open(); //Seri haberleşmeyi başlat
+                    label1.Text = "Bağlanıldı"; // combobox altındaki yere bilgi yaz
+                    timer1.Start(); // Sürekli işlem yapmak için timer başlat
+                    gonderilecekyazi = "Guncelleme"; //WeMos a gönderilecek komutun yazdırılması
+                    serialPort1.WriteLine(gonderilecekyazi); //gonderilecekyazıyı WeMos a gönder.
+                }
+            }
+            else
+            {
+                MessageBox.Show("Önce bağlantıyı kesmelisiniz");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (serialPort1.IsOpen) //Bağlantı var ise işlem yaptır.
+            {
+                timer1.Stop(); //Sürekli işlemi bitir.
+                serialPort1.Close(); //Seri haberleşme bağlantısını kapat.
+                label1.Text = "Bağlantı Kesildi";
+                label13.Text = "Çiçek bilgisayara bağlı değil"; //Bağlantı durumunu yazdır.
+                cicekinternetebagli = 0; //İnternete bağlı olmadığını göster.
+            }
+        }
+        string gonderilecekyazi; //Gönderilecek yazıyı tutan değişken.
+        private void button3_Click(object sender, EventArgs e) //Yeni Wi-Fi gönderme butonu
+        {
+            if (serialPort1.IsOpen)
+            {
+                gonderilecekyazi = "Wifi: " + textBox1.Text; //Textboxtaki bilgiyi Wifi: ile birleştir gönder.
+                serialPort1.WriteLine(gonderilecekyazi); //WriteLine ile en sona \n ekleniyor. Bu sayede mesajın bittiği bildiriliyor.
+                label13.Text = "Çiçek internete bağlı değil";
+                cicekinternetebagli = 0;
+            }
+        
+        }
+
+        private void button4_Click(object sender, EventArgs e) //Yeni Wi-Fi Şifresi gönderme butonu
+        {
+            if (serialPort1.IsOpen)
+            {
+                gonderilecekyazi = "Sifre: " + textBox2.Text;
+                serialPort1.WriteLine(gonderilecekyazi);
+                label13.Text = "Çiçek internete bağlı değil";
+                cicekinternetebagli = 0;
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e) //Sürekli çalışma
+        {
+            try
+            {
+                string sonuc = serialPort1.ReadExisting(); //Seri haberleşmen gelen bilgileri çekip sonuca yazdır.
+                if (sonuc.IndexOf("Wi-Fi") >= 0 && sonuc.IndexOf("Baglanilan") >= 0) //Wi-Fi ve Baglanilan var ise sonuc değişkeninde içeri gir
+                {
+                    label4.Text = sonuc + ""; //Bağlanılan Wi-Fi bilgisini ekrana yazdır.
+                }
+                else if (sonuc.IndexOf("Wi-Fi") >= 0 && sonuc.IndexOf("Sifresi") >= 0)
+                {
+                    label5.Text = sonuc + ""; //Bağlanılan Wi-Fi Şifre bilgisini ekrana yazdır.
+                }
+                else if (sonuc.IndexOf("Wifi") >= 0 && sonuc.IndexOf("Bagli") >= 0)
+                {
+                    label13.Text = "Çiçek internete bağlı"; //Çiçek durumunu yazdır.
+                    cicekinternetebagli = 1;
+                }
+                else if (sonuc.IndexOf("Wifi") >= 0 && sonuc.IndexOf("Degil") >= 0)
+                {
+                    label13.Text = "Çiçek internete bağlı değil";
+                    cicekinternetebagli = 0;
+                }
+                label2.Text = sonuc + "";
+            }
+            catch (Exception ex)
+            {
+                timer1.Stop();
+                if (serialPort1.IsOpen == true)
+                {
+                    serialPort1.Close();
+                }
+                MessageBox.Show(ex.Message);
+                
+                
+                throw;
+            }
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e) //Program kapatıldı ise
+        {
+            if (serialPort1.IsOpen == true) //Bağlantı var ise bağlantıyı kes.
+            {
+                serialPort1.Close();
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e) //Güncelle butonu
+        {
+            if (serialPort1.IsOpen) //Seri haberleşme var ise.
+            {
+                gonderilecekyazi = "Guncelleme"; //Guncelleme bilgisini WeMos a gönder.
+                serialPort1.WriteLine(gonderilecekyazi);
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e) //WeMos a kontrol komutu gönder.
+        {
+            if (serialPort1.IsOpen)
+            {
+                if (cicekinternetebagli == 1)
+                {
+                    gonderilecekyazi = "Kontrol";
+                    serialPort1.WriteLine(gonderilecekyazi);
+                }
+                else //İnternete bağlı değil ise çiçek işlem yapmayacaktır. Bunu anlamak için yapılan sorgu.
+                {
+                    MessageBox.Show("Bu komutun çalışabilmesi için çiçeğin internete bağlı olması gerekmektedir.");
+                }
+            }
+        }
+
+    }
+}
